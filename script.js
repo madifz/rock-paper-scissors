028d4a25064cc39821b547615094898fe49a31ea
Danial Faizsal <m.d.faizsal@gmail.com>
2021-07-07 17:42:20 +0800

    Completed first iteration of rock, paper, scissors game with all requirements fulfilled (slight chage with playRound returning console.log instead of just a return)

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let computerSelection = "";
+let playerSelection = "";
+
+game();
+
 function computerPlay() {
     let compStr = "";
     let randomNum = Math.floor((Math.random() * 3) + 1); 
@@ -9,12 +14,49 @@ function computerPlay() {
     return compStr;
 }
 
-function playRound(playerSelection, computerSelection) {
-    let computerSelection = computerPlay();
-    let playerSelection = prompt("Please choose: Rock, Paper or Scissor:").toLowerCase();
-    if (playerSelection !== "rock" || "paper" || "scissors") {
-        alert("Wrong input, please try again!");
-        playerSelection = prompt("Please choose: Rock, Paper or Scissor:").toLowerCase();
-    } else break;
-    //return `You Lose! ${computerSelection} beats ${playerSelection}`;
+function playRound() {
+    computerSelection = computerPlay();
+    playerSelection = prompt("Please choose: Rock, Paper or Scissor:").toLowerCase();
+    //while ((playerSelection !== "rock") || (playerSelection !== "paper") || (playerSelection !== "scissors")) {
+        //alert("Wrong input, please try again!");
+        //playerSelection = prompt("Please choose: Rock, Paper or Scissor:").toLowerCase();
+    //};
+
+    if (playerSelection === "rock" && computerSelection === "rock" ||
+    playerSelection === "paper" && computerSelection === "paper" || 
+    playerSelection === "scissors" && computerSelection === "scissors") {
+        console.log(`It's a draw! You and the computer chose ${playerSelection}!`);
+    } else if (playerSelection === "rock" && computerSelection === "scissors" ||
+    playerSelection === "paper" && computerSelection === "rock" || 
+    playerSelection === "scissors" && computerSelection === "paper") {
+        return console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
+    } else console.log(`You lose! ${computerSelection} beats ${playerSelection}!`);
+}
+
+function game() {
+    let gameRounds = 5;
+    let playerWins = 0;
+    let computerWins = 0;
+    for (let i = 1; i <= gameRounds; i++) {
+        playRound();
+        if (playerSelection === "rock" && computerSelection === "rock" ||
+        playerSelection === "paper" && computerSelection === "paper" || 
+        playerSelection === "scissors" && computerSelection === "scissors") {
+            console.log(`No winner, it's a draw! Round ${i} tie!`);
+        } else if (playerSelection === "rock" && computerSelection === "scissors" ||
+        playerSelection === "paper" && computerSelection === "rock" || 
+        playerSelection === "scissors" && computerSelection === "paper") {
+            playerWins ++;
+            console.log(`You win round ${i}!`);
+        } else {
+            computerWins ++;
+            console.log(`You lose round ${i}`);
+        }
+    }
+
+    if (computerWins > playerWins) {
+        console.log(`Computer beats you with ${computerWins} wins to ${playerWins}, this game of ${gameRounds} rounds ends with computer's win!`);
+    } else if (computerWins < playerWins) {
+        console.log(`You beat computer with ${playerWins} wins to ${computerWins}, this game of ${gameRounds} rounds ends with your win!`);
+    } else console.log(`Both you and computer has ${playerWins} wins, this game of ${gameRounds} rounds is a draw!`);
 }
